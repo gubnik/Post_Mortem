5e748eddae404972e9ca42d3765b78163e468d4b
nikgub_ <danger8217.nik@gmail.com>
2023-05-01 19:33:48 +0300

    Added ModItems class

--- src/main/java/net/team_prometheus/post_mortem/Post_Mortem.java
+++ src/main/java/net/team_prometheus/post_mortem/Post_Mortem.java
@@ -9,6 +9,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.team_prometheus.post_mortem.item.ModItems;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -19,6 +20,8 @@ public class Post_Mortem
     private static final Logger LOGGER = LogUtils.getLogger();
     public Post_Mortem() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
---
+++ src/main/java/net/team_prometheus/post_mortem/item/ModItems.java
@@ -0,0 +1,20 @@
+package net.team_prometheus.post_mortem.item;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.team_prometheus.post_mortem.Post_Mortem;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+        DeferredRegister.create(ForgeRegistries.ITEMS, Post_Mortem.MOD_ID);
+
+    public static final RegistryObject<Item> TOMBSTONE_SHARD = ITEMS.register("tombstone_shard",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS)));
+    public static void register (IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+}
