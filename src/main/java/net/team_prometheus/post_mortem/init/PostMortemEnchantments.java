5b7b152aa8a44e97e3f5d8e36a318531b7f03309
nikgub_ <danger8217.nik@gmail.com>
2023-05-04 01:14:40 +0300

    Simplified enchantments

--- src/main/java/net/team_prometheus/post_mortem/init/PostMortemEnchantments.java
+++ src/main/java/net/team_prometheus/post_mortem/init/PostMortemEnchantments.java
@@ -7,10 +7,13 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.team_prometheus.post_mortem.Post_Mortem;
-import net.team_prometheus.post_mortem.enchantment.Rupture;
 
 public class PostMortemEnchantments {
     public static final DeferredRegister<Enchantment> REGISTRY = DeferredRegister.create(ForgeRegistries.ENCHANTMENTS, Post_Mortem.MOD_ID);
-    public static final RegistryObject<Enchantment> RUPTURE = REGISTRY.register("rupture", () -> new Rupture());
+    public static final RegistryObject<Enchantment> RUPTURE = REGISTRY.register("rupture", () -> new Enchantment(Enchantment.Rarity.COMMON, EnchantmentCategory.WEAPON, new EquipmentSlot[]{}) {
+        public int getMaxLevel(){
+            return 5;
+        }
+    });
 
 }
