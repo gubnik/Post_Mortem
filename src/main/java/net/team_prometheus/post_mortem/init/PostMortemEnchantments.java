6a164311b34f40c2b53ff0c5fbf657828e15bbf3
nikgub_ <danger8217.nik@gmail.com>
2023-05-04 16:27:00 +0300

    ...

--- src/main/java/net/team_prometheus/post_mortem/init/PostMortemEnchantments.java
+++ src/main/java/net/team_prometheus/post_mortem/init/PostMortemEnchantments.java
@@ -1,12 +1,18 @@
 package net.team_prometheus.post_mortem.init;
 
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.team_prometheus.post_mortem.Post_Mortem;
+
+import java.util.Random;
 
 public class PostMortemEnchantments {
     public static final DeferredRegister<Enchantment> REGISTRY = DeferredRegister.create(ForgeRegistries.ENCHANTMENTS, Post_Mortem.MOD_ID);
@@ -15,5 +21,4 @@ public class PostMortemEnchantments {
             return 5;
         }
     });
-
 }
