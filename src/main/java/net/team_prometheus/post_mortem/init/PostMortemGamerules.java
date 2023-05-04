fc862ac8c37a02c07c024c21ccfd015091760eaa
nikgub_ <danger8217.nik@gmail.com>
2023-05-04 16:26:41 +0300

    Deleted DEFAULT_BLEEDING_APPLICATION gamerule due to it being useless

--- src/main/java/net/team_prometheus/post_mortem/init/PostMortemGamerules.java
+++ src/main/java/net/team_prometheus/post_mortem/init/PostMortemGamerules.java
@@ -7,7 +7,6 @@ import net.minecraft.world.level.GameRules;
 public class PostMortemGamerules {
     public static final GameRules.Key<GameRules.BooleanValue> UNIVERSAL_BLEEDING = GameRules.register("universalBleeding", GameRules.Category.PLAYER, GameRules.BooleanValue.create(true));
     public static final GameRules.Key<GameRules.IntegerValue> BLEEDING_ACTIVATION = GameRules.register("bleedingActivation", GameRules.Category.PLAYER, GameRules.IntegerValue.create(4));
-    public static final GameRules.Key<GameRules.IntegerValue> DEFAULT_BLEEDING_APPLICATION = GameRules.register("defaulBleedingApplication", GameRules.Category.PLAYER, GameRules.IntegerValue.create(1));
     public static final GameRules.Key<GameRules.IntegerValue> BLEEDING_DAMAGE = GameRules.register("bleedingDamage", GameRules.Category.PLAYER, GameRules.IntegerValue.create(10));
     public static final GameRules.Key<GameRules.BooleanValue> GHOSTS_SPAWN_AFTER_DEATH = GameRules.register("ghostsSpawnAfterDeath", GameRules.Category.SPAWNING, GameRules.BooleanValue.create(true));
 }
