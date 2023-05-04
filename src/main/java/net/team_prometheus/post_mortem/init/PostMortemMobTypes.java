845c61920ba81d8682cac30526946e2fe9980144
nikgub_ <danger8217.nik@gmail.com>
2023-05-05 01:17:14 +0300

    Deleted unnecessary heritage

--- src/main/java/net/team_prometheus/post_mortem/init/PostMortemMobTypes.java
+++ src/main/java/net/team_prometheus/post_mortem/init/PostMortemMobTypes.java
@@ -2,6 +2,6 @@ package net.team_prometheus.post_mortem.init;
 
 import net.minecraft.world.entity.MobType;
 
-public class PostMortemMobTypes extends MobType {
+public class PostMortemMobTypes {
     public static final MobType GHOST = new MobType();
 }
