3ed98dd80351de429e60e4e665b7567c958f8c14
nikgub_ <danger8217.nik@gmail.com>
2023-05-04 23:53:19 +0300

    Added Exorcism enchantment

--- src/main/java/net/team_prometheus/post_mortem/init/PostMortemEnchantments.java
+++ src/main/java/net/team_prometheus/post_mortem/init/PostMortemEnchantments.java
@@ -1,10 +1,11 @@
 package net.team_prometheus.post_mortem.init;
 
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.MobType;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
 import net.minecraftforge.registries.DeferredRegister;
@@ -12,13 +13,30 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.team_prometheus.post_mortem.Post_Mortem;
 
-import java.util.Random;
-
 public class PostMortemEnchantments {
     public static final DeferredRegister<Enchantment> REGISTRY = DeferredRegister.create(ForgeRegistries.ENCHANTMENTS, Post_Mortem.MOD_ID);
     public static final RegistryObject<Enchantment> RUPTURE = REGISTRY.register("rupture", () -> new Enchantment(Enchantment.Rarity.COMMON, EnchantmentCategory.WEAPON, new EquipmentSlot[]{}) {
         public int getMaxLevel(){
             return 5;
+        }
+        @Override
+        public void doPostAttack(LivingEntity attacker, Entity target, int level) {
+            if(target instanceof LivingEntity _entity)
+                if (Math.random() < level * 0.2) {
+                    _entity.addEffect(new MobEffectInstance(PostMortemEffects.BLEEDING.get(), 100,
+                            (int) ((_entity.hasEffect(PostMortemEffects.BLEEDING.get()) ? _entity.getEffect(PostMortemEffects.BLEEDING.get()).getAmplifier() : 0) + 1)));
+            } // rupture
+        }
+    });
+    public static final RegistryObject<Enchantment> EXORCISM = REGISTRY.register("exorcism", () -> new Enchantment(Enchantment.Rarity.RARE, EnchantmentCategory.WEAPON, new EquipmentSlot[]{}) {
+        public int getMaxLevel(){
+            return 3;
+        }
+        @Override
+        public float getDamageBonus(int level, MobType mobType, ItemStack enchantedItem){
+            if(mobType == PostMortemMobTypes.GHOST) {
+                return (float) (level * 0.4);
+            } else return 0f;
         }
     });
 }
