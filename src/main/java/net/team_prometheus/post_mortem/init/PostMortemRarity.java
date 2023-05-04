0b01f56ad6beaed6fbae8b2c63092321e786e117
nikgub_ <danger8217.nik@gmail.com>
2023-05-04 21:22:34 +0300

    ...

--- src/main/java/net/team_prometheus/post_mortem/init/PostMortemRarity.java
+++ src/main/java/net/team_prometheus/post_mortem/init/PostMortemRarity.java
@@ -1,13 +1,17 @@
 package net.team_prometheus.post_mortem.init;
 
 import net.minecraft.ChatFormatting;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Rarity;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.team_prometheus.post_mortem.Post_Mortem;
 
 public class PostMortemRarity{
     public static Rarity BloodRarity() {
         return Rarity.create("BLOOD", ChatFormatting.DARK_RED);
     }
     public static Rarity SoulflameRarity() {
-        return Rarity.create("BLOOD", ChatFormatting.DARK_AQUA);
+        return Rarity.create("SOULFLAME", ChatFormatting.DARK_AQUA);
     }
 }
--- src/main/java/net/team_prometheus/post_mortem/item/ModItems.java
+++ src/main/java/net/team_prometheus/post_mortem/item/ModItems.java
@@ -7,6 +7,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.team_prometheus.post_mortem.Post_Mortem;
+import net.team_prometheus.post_mortem.init.PostMortemRarity;
 import net.team_prometheus.post_mortem.init.PostMortemTabs;
 import net.team_prometheus.post_mortem.item.curio_items.BloodPact;
 import net.team_prometheus.post_mortem.item.echoes_weapons.EchoesTooltipProvider;
@@ -18,18 +19,27 @@ public class ModItems {
         DeferredRegister.create(ForgeRegistries.ITEMS, Post_Mortem.MOD_ID);
     public static final RegistryObject<Item> TOMBSTONE_SHARD = ITEMS.register("tombstone_shard",
             () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS)));
+    public static final RegistryObject<Item> HAUNTED_SPLINTER = ITEMS.register("haunted_splinter",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS)));
     public static final RegistryObject<Item> SPIRIT_FRAGMENT = ITEMS.register("spirit_fragment",
-            () -> new Item(new Item.Properties().tab(PostMortemTabs.SOULBENDING_TAB)));
+            () -> new Item(new Item.Properties().tab(PostMortemTabs.SOULBENDING_TAB).rarity(PostMortemRarity.SoulflameRarity())));
+    public static final RegistryObject<Item> SOULFILLED_INGOT = ITEMS.register("soulfilled_ingot",
+            () -> new Item(new Item.Properties().tab(PostMortemTabs.SOULBENDING_TAB).rarity(PostMortemRarity.SoulflameRarity())));
     public static final RegistryObject<Item> UNBOUND_BLOOD = ITEMS.register("unbound_blood",
-            () -> new Item(new Item.Properties().tab(PostMortemTabs.SANGUIMANCY_TAB)));
+            () -> new Item(new Item.Properties().tab(PostMortemTabs.SANGUIMANCY_TAB).rarity(PostMortemRarity.BloodRarity())));
+    public static final RegistryObject<Item> BLOODSTAINED_INGOT = ITEMS.register("bloodstained_ingot",
+            () -> new Item(new Item.Properties().tab(PostMortemTabs.SANGUIMANCY_TAB).rarity(PostMortemRarity.BloodRarity())));
 
     // weapons, echo variants
     public static final RegistryObject<Item> BLOODLETTER = ITEMS.register("bloodletter",
-            () -> new EchoesTooltipProvider(5, -2.4f, 666, 14, 0f, 4, 17, Ingredient.of(ModItems.UNBOUND_BLOOD.get()), PostMortemTabs.SANGUIMANCY_TAB));
+            () -> new EchoesTooltipProvider(5, -2.4f, 1666, 14, 0f, 4, 14, Ingredient.of(ModItems.UNBOUND_BLOOD.get()), PostMortemTabs.SANGUIMANCY_TAB));
+    public static final RegistryObject<Item> SOULFLAME_SPEAR = ITEMS.register("soulflame_spear",
+            () -> new EchoesTooltipProvider(6, -2.6f, 1777, 14, 0f, 4, 17, Ingredient.of(ModItems.SPIRIT_FRAGMENT.get()), PostMortemTabs.SOULBENDING_TAB));
 
     // curios items
     public static final RegistryObject<Item> BLOOD_PACT = ITEMS.register("blood_pact",
             BloodPact::new);
+
     public static void register (IEventBus eventBus){
         ITEMS.register(eventBus);
     }
