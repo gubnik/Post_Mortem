f2b676cafcc29082011b688aeadb7fae5b00a0fc
nikgub_ <danger8217.nik@gmail.com>
2023-05-04 16:44:02 +0300

    Optimized imports

--- src/main/java/net/team_prometheus/post_mortem/item/curio_items/BloodPact.java
+++ src/main/java/net/team_prometheus/post_mortem/item/curio_items/BloodPact.java
@@ -1,19 +1,9 @@
 package net.team_prometheus.post_mortem.item.curio_items;
 
 import top.theillusivec4.curios.api.type.capability.ICurioItem;
-
 import net.team_prometheus.post_mortem.init.PostMortemTabs;
-
-import net.minecraft.world.level.Level;
-import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.Rarity;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
-import net.minecraft.network.chat.Component;
-
-import java.util.List;
-import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.ChatFormatting;
 
 public class BloodPact extends Item implements ICurioItem {
     public BloodPact() {
