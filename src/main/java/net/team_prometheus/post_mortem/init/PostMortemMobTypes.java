70ce1e841f03eea0e31c95b4914eaa7d3fa388ed
nikgub_ <danger8217.nik@gmail.com>
2023-05-04 23:52:50 +0300

    Added GHOST mob type

---
+++ src/main/java/net/team_prometheus/post_mortem/init/PostMortemMobTypes.java
@@ -0,0 +1,7 @@
+package net.team_prometheus.post_mortem.init;
+
+import net.minecraft.world.entity.MobType;
+
+public class PostMortemMobTypes extends MobType {
+    public static final MobType GHOST = new MobType();
+}
