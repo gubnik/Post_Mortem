a9530dfc344c26e0ed177d855fc3942371210000
nikgub_ <danger8217.nik@gmail.com>
2023-05-05 17:39:31 +0300

    Added Bloody Slash echo

---
+++ src/main/java/net/team_prometheus/post_mortem/echoes_weapons/attacks/BloodySlash.java
@@ -0,0 +1,41 @@
+package net.team_prometheus.post_mortem.echoes_weapons.attacks;
+
+import net.team_prometheus.post_mortem.init.ParticleTypes;
+import net.team_prometheus.post_mortem.init.PostMortemDamageSource;
+import net.team_prometheus.post_mortem.init.PostMortemEffects;
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.server.level.ServerLevel;
+import java.util.Objects;
+import java.util.List;
+import java.util.Comparator;
+
+public class BloodySlash {
+    public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
+        if (entity == null) return;
+        double pitch, yaw;
+        pitch = entity.getYRot() - 60;
+        yaw = entity.getXRot() - 60;
+        for (int index0 = 0; index0 < 120; index0++) {
+            if (world instanceof ServerLevel _level)
+                _level.sendParticles((ParticleTypes.DROP_OF_BLOOD.get()), (entity.getX() - Math.sin(Math.toRadians(yaw)) * 2), ((entity.getY() + 1.5) - Math.sin(Math.toRadians(pitch)) * 2),
+                        (entity.getZ() + Math.cos(Math.toRadians(yaw)) * 2), 2, 0.1, 0.1, 0.1, 0.3);
+            yaw += 1;
+            pitch += 1;
+        }
+        final Vec3 _center = new Vec3((x + entity.getLookAngle().x * 2), (y + entity.getLookAngle().y * 2 + 1.4), (z + entity.getLookAngle().z * 2));
+        List<Entity> _entfound = world.getEntitiesOfClass(Entity.class, new AABB(_center, _center).inflate(4 / 2d), e -> true).stream().sorted(Comparator.comparingDouble(_entcnd -> _entcnd.distanceToSqr(_center))).toList();
+        for (Entity entityiterator : _entfound) {
+            if (!(entityiterator == entity) && entityiterator instanceof LivingEntity _entity) {
+                _entity.hurt(PostMortemDamageSource.BLEED, 4);
+                _entity.addEffect(new MobEffectInstance(PostMortemEffects.BLEEDING.get(), 80,
+                        (_entity.hasEffect(PostMortemEffects.BLEEDING.get()) ? Objects.requireNonNull(_entity.getEffect(PostMortemEffects.BLEEDING.get())).getAmplifier() + 1 : 0), (false),
+                        (true)));
+            }
+        }
+    }
+}
---
+++ src/main/java/net/team_prometheus/post_mortem/echoes_weapons/triggers/BloodySlashTrigger.java
@@ -0,0 +1,35 @@
+package net.team_prometheus.post_mortem.echoes_weapons.triggers;
+
+import dev.kosmx.playerAnim.api.AnimUtils;
+import dev.kosmx.playerAnim.api.layered.KeyframeAnimationPlayer;
+import dev.kosmx.playerAnim.minecraftApi.PlayerAnimationRegistry;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.LevelAccessor;
+import net.team_prometheus.post_mortem.Post_Mortem;
+import net.team_prometheus.post_mortem.echoes_weapons.attacks.BloodySlash;
+import net.team_prometheus.post_mortem.init.SetupAnimations;
+
+import java.util.Objects;
+
+public class BloodySlashTrigger {
+    public static void execute(LevelAccessor world, double x, double y, double z, Entity entity, ItemStack item) {
+        if (entity == null)
+            return;
+        if(entity instanceof Player player)
+            if (player.getHealth() - 2 > 0 || (player.getAbilities().instabuild)) {
+                if (!(player.getAbilities().instabuild)) {
+                    player.setHealth(((player.getHealth()) - 2));
+                }
+                player.getCooldowns().addCooldown(item.getItem(), 60);
+                var animation = SetupAnimations.animationData.get(player);
+                if (animation != null) {
+                    animation.setAnimation(new KeyframeAnimationPlayer(Objects.requireNonNull(PlayerAnimationRegistry.getAnimation(new ResourceLocation("post_mortem", "bloody_slash")))));
+                }
+                AnimUtils.disableFirstPersonAnim = false;
+                Post_Mortem.queueServerWork(4, () -> BloodySlash.execute(world, x, y, z, entity));
+        }
+    }
+}
