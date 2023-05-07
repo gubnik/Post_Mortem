4b2d711cc29da1a68290aeeb47a91bdd4f102765
nikgub_ <danger8217.nik@gmail.com>
2023-05-08 01:00:01 +0300

    ...

--- src/main/java/net/team_prometheus/post_mortem/potion/BleedingApplication.java
+++ src/main/java/net/team_prometheus/post_mortem/potion/BleedingApplication.java
@@ -23,31 +23,30 @@ public class BleedingApplication {
             execute(event.getEntity().level, event.getEntity(), event.getSource().getDirectEntity(), event.getAmount());
         }
     }
-
     public static void execute(LevelAccessor world, Entity entity, Entity immediatesourceentity, double amount) {
         if (entity == null || immediatesourceentity == null)
             return;
         double lvl;
         double time;
-        if(immediatesourceentity instanceof LivingEntity _livEnt && entity instanceof LivingEntity _entity)
+        if(immediatesourceentity instanceof LivingEntity source && entity instanceof LivingEntity target)
             // bleed weapons
-            if (!entity.isInvulnerable() && !(_entity.isBlocking()) && entity.isAlive()) {
-                if (_livEnt.getMainHandItem().is(ItemTags.create(new ResourceLocation("forge:bleed_weapons")))) {
+            if (!entity.isInvulnerable() && !(target.isBlocking()) && entity.isAlive()) {
+                if (source.getMainHandItem().is(ItemTags.create(new ResourceLocation("forge:bleed_weapons")))) {
                     if (amount >= 3) {
-                        _entity.addEffect(new MobEffectInstance(PostMortemEffects.BLEEDING.get(), 100,
-                            ((_entity.hasEffect(PostMortemEffects.BLEEDING.get()) ? _entity.getEffect(PostMortemEffects.BLEEDING.get()).getAmplifier() : 0) + 1)));
+                        target.addEffect(new MobEffectInstance(PostMortemEffects.BLEEDING.get(), 100,
+                            ((target.hasEffect(PostMortemEffects.BLEEDING.get()) ? target.getEffect(PostMortemEffects.BLEEDING.get()).getAmplifier() : 0) + 1)));
                 }
             }
-            if ((_entity.hasEffect(PostMortemEffects.BLEEDING.get()) ? _entity.getEffect(PostMortemEffects.BLEEDING.get()).getAmplifier() : 0) == (world.getLevelData().getGameRules()
+            if ((target.hasEffect(PostMortemEffects.BLEEDING.get()) ? target.getEffect(PostMortemEffects.BLEEDING.get()).getAmplifier() : 0) == (world.getLevelData().getGameRules()
                     .getInt(PostMortemGameRules.BLEEDING_ACTIVATION))) {
-                if (CuriosApi.getCuriosHelper().findFirstCurio(_livEnt, ModItems.BLOOD_PACT.get()).isPresent()) {
-                    if (!_livEnt.hasEffect(MobEffects.DAMAGE_BOOST) || (_livEnt.hasEffect(MobEffects.DAMAGE_BOOST) ? _livEnt.getEffect(MobEffects.DAMAGE_BOOST).getAmplifier() : 0) <= 1) {
-                        _livEnt.addEffect(new MobEffectInstance(MobEffects.DAMAGE_BOOST, 200, 1, (false), (true)));
+                if (CuriosApi.getCuriosHelper().findFirstCurio(source, ModItems.BLOOD_PACT.get()).isPresent()) {
+                    if (!source.hasEffect(MobEffects.DAMAGE_BOOST) || (source.hasEffect(MobEffects.DAMAGE_BOOST) ? source.getEffect(MobEffects.DAMAGE_BOOST).getAmplifier() : 0) <= 1) {
+                        source.addEffect(new MobEffectInstance(MobEffects.DAMAGE_BOOST, 200, 1, (false), (true)));
                     } else {
-                        lvl = _livEnt.hasEffect(MobEffects.DAMAGE_BOOST) ? _livEnt.getEffect(MobEffects.DAMAGE_BOOST).getAmplifier() : 0;
-                        time = _livEnt.hasEffect(MobEffects.DAMAGE_BOOST) ? _livEnt.getEffect(MobEffects.DAMAGE_BOOST).getDuration() : 0;
-                        _livEnt.removeEffect(MobEffects.DAMAGE_BOOST);
-                        _livEnt.addEffect(new MobEffectInstance(MobEffects.DAMAGE_BOOST, (int) (time + 200), (int) lvl, (false), (true)));
+                        lvl = source.hasEffect(MobEffects.DAMAGE_BOOST) ? source.getEffect(MobEffects.DAMAGE_BOOST).getAmplifier() : 0;
+                        time = source.hasEffect(MobEffects.DAMAGE_BOOST) ? source.getEffect(MobEffects.DAMAGE_BOOST).getDuration() : 0;
+                        source.removeEffect(MobEffects.DAMAGE_BOOST);
+                        source.addEffect(new MobEffectInstance(MobEffects.DAMAGE_BOOST, (int) (time + 200), (int) lvl, (false), (true)));
                     }
                 }
             } // blood pact
