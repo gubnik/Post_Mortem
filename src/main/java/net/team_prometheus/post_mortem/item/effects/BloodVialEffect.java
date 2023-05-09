124ed2c04b41a7599e91591d48b75c9b51567915
nikgub_ <danger8217.nik@gmail.com>
2023-05-10 00:20:23 +0300

    ...

--- src/main/java/net/team_prometheus/post_mortem/item/effects/BloodVialEffect.java
+++ src/main/java/net/team_prometheus/post_mortem/item/effects/BloodVialEffect.java
@@ -4,6 +4,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
@@ -24,9 +25,12 @@ public class BloodVialEffect {
             ItemStack itemStack = event.getItem();
             LivingEntity entity = event.getEntity();
             LevelAccessor world = event.getEntity().getLevel();
-            double x = event.getEntity().getX(); double y = event.getEntity().getY(); double z = event.getEntity().getZ();
+            double x = event.getEntity().getX();
+            double y = event.getEntity().getY();
+            double z = event.getEntity().getZ();
             if (itemStack.getItem() == ModItems.BLOOD_VIAL.get()) {
-                entity.addEffect(new MobEffectInstance(PostMortemEffects.BLEEDING.get(), 100, entity.hasEffect(PostMortemEffects.BLEEDING.get()) ? Objects.requireNonNull(entity.getEffect(PostMortemEffects.BLEEDING.get())).getAmplifier() + 1: 0));
+                entity.addEffect(new MobEffectInstance(PostMortemEffects.BLEEDING.get(), 100, entity.hasEffect(PostMortemEffects.BLEEDING.get()) ? Objects.requireNonNull(entity.getEffect(PostMortemEffects.BLEEDING.get())).getAmplifier() + 1 : 0));
+                entity.addEffect(new MobEffectInstance(MobEffects.HEAL, 1, 1));
                 Level level = (Level) world;
                 if (!level.isClientSide()) {
                     level.playSound(null, new BlockPos(x, y, z), Objects.requireNonNull(ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("block.glass.break"))), SoundSource.PLAYERS, (float) 0.6, 1);
